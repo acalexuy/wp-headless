1e26853408ae011205a7ff6a09602b7c43680af0
Alex Uy <alex.uy@canstar.com.au>
2022-09-18 14:18:59 +0800

    enable featured image

--- wp-content/themes/wp-headless/inc/misc/helpers.php
+++ wp-content/themes/wp-headless/inc/misc/helpers.php
@@ -1,3 +1,6 @@
 <?php
+    // Disable gutenberg editor
     add_filter('use_block_editor_for_post_type', '__return_false', 10);
+    // enable featured image on post
+    add_theme_support( 'post-thumbnails' );
 ?>
